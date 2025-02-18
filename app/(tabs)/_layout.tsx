5adc1d362f1f20222ef0f392b137e63f6158d919
Tutorials-Git <laicheongkian@gmail.com>
2025-02-18 23:18:12 +0800

    change to jungle

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,7 +29,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Sweet Home',
+          title: 'jungle Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
